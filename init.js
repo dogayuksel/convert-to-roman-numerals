4ede82be49cc698f7318bef5dc9af448b3c26108
Doga Yuksel <dogayuksel@gmail.com>
2018-11-06 22:15:11 +0100

    feat: convert numbers smaller than ten to numerals
    Implements convertToRomanNumeral function to iterate over given number
    by deducing largest possible chunk of value until remainder is zero.
    Corresponding roman numeral symbols are concatanated to form the
    output.

--- init.js
+++ init.js
@@ -1,5 +1,30 @@
+const romanSymbols = [
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I'],
+];
+
+/**
+ * Converts given number to roman numerals.
+ * @param number [number] Number to be converted
+ * @return [string] Corresponding roman numeral value
+ */
 const convertToRomanNumeral = (number) => {
-  return number;
+  let remainder = number;
+  let romanNumeral = '';
+  // On each step
+  // Deducts largest possible chunk of value
+  // and appends the corresponding symbol(s) to the output
+  // until remainder is zero
+  while (remainder > 0) {
+    const [value, symbol] = romanSymbols.filter(([value, symbol]) => {
+      return value <= remainder;
+    })[0];
+    remainder = remainder - value;
+    romanNumeral = `${romanNumeral}${symbol}`;
+  }
+  return romanNumeral;
 };
 
 module.exports = { convertToRomanNumeral };
