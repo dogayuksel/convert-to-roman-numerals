ee16523e5f800746e399bf67206253d0c74b3f75
Doga Yuksel <dogayuksel@gmail.com>
2018-11-06 22:45:02 +0100

    test: add cases for integers below 5000 as inputs

--- test-init.js
+++ test-init.js
@@ -9,11 +9,21 @@ const assert = (a, b) => {
   }
 }
 
-// Inputs smaller then 10
+// Inputs smaller than 10
 assert(convertToRomanNumeral(1), 'I');
 assert(convertToRomanNumeral(3), 'III');
 assert(convertToRomanNumeral(4), 'IV');
 assert(convertToRomanNumeral(7), 'VII');
 assert(convertToRomanNumeral(9), 'IX');
+
+// Inputs equal or larger than 10 smaller than 5000
+assert(convertToRomanNumeral(10), 'X');
+assert(convertToRomanNumeral(39), 'XXXIX');
+assert(convertToRomanNumeral(246), 'CCXLVI');
+assert(convertToRomanNumeral(1776), 'MDCCLXXVI');
+assert(convertToRomanNumeral(1954), 'MCMLIV');
+assert(convertToRomanNumeral(1990), 'MCMXC');
+assert(convertToRomanNumeral(2014), 'MMXIV');
+assert(convertToRomanNumeral(4449), 'MMMMCMXCI');
 
 console.log('All Tests Passed!');
